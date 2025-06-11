9fb9eeba481e0f90c6084d0db030c7e6d966aab9
alm35943 <malek.alsibai@st.oth-regensburg.de>
2025-06-11 20:09:36 +0200

    another example to use static (calc)

--- OOP_EL/10StaticMethodsAndFields/src/Main.java
+++ OOP_EL/10StaticMethodsAndFields/src/Main.java
@@ -2,14 +2,9 @@ public class Main {
     public static void main(String[] args) {
 
     Car c1 = new Car();
-
-
     Car c2 = new Car();
-
     Car c3 = new Car();
-
     Car c4 = new Car();
-
     Car c5 = new Car();
 
     System.out.println(Car.getNo_of_objects());
