5910afd21e0b6153848283f7543bb777e6a922bd
alm35943 <malek.alsibai03@gmail.com>
2025-06-16 19:02:07 +0200

    differences between parameteres and instances

--- OOP_EL/11PassingObjectToVariableRectangleEX/src/Main.java
+++ OOP_EL/11PassingObjectToVariableRectangleEX/src/Main.java
@@ -2,9 +2,13 @@ public class Main {
     public static void main(String[] args) {
 
         Rectangle room1 = new Rectangle(); //object room1 has been created from Rectangle class
+
         double roomLength = 50;
-        room1.setLength(roomLength);
+        room1.setLength(roomLength); // we don't send the roomLength variable itself. we send a copy of the save value at roomLength
+
         System.out.println(roomLength);
+
+
 
     }
 }
