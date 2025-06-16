13ab4cc90df2921002516b3b27cc40eb8b606daa
alm35943 <malek.alsibai03@gmail.com>
2025-06-16 20:41:44 +0200

    an object room3 modifies object MyInputObject. basically an object modifying another object

--- OOP_EL/11PassingobjectToVariableRectangleEx2/src/Main.java
+++ OOP_EL/11PassingobjectToVariableRectangleEx2/src/Main.java
@@ -3,6 +3,23 @@ public class Main {
 
         Rectangle room3 = new Rectangle();
 
+        Rectangle myInputObject = new Rectangle(10,30); // parameter
+
+
+        room3.modifyObject(myInputObject);
+
+       System.out.println(myInputObject.getLength());
+       System.out.println(myInputObject.getWidth());
+
+        System.out.println(room3.getLength());
+        System.out.println(room3.getWidth());
+
+
+
+
+
 
     }
 }
+
+
