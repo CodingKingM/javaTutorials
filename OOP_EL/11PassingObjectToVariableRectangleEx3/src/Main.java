17cf4c69b0f9c3a7ef8339f99f61102bbb018e4d
alm35943 <malek.alsibai03@gmail.com>
2025-06-16 21:37:29 +0200

    working optimised combining two objects r1 and r2 code

--- OOP_EL/11PassingObjectToVariableRectangleEx3/src/Main.java
+++ OOP_EL/11PassingObjectToVariableRectangleEx3/src/Main.java
@@ -4,15 +4,9 @@ public class Main {
         Rectangle r1 = new Rectangle(10,50);
         Rectangle r2 = new Rectangle(30, 70);
         // to combine two objects r1 and r2
-        Rectangle r3 =  r1.addObject(r2); //
-        System.out.println(r3.getLength());
-        System.out.println(r3.getWidth());
-
-
-
-
-
-
+       // Rectangle r3 =  r1.addObject(r2); //
+        System.out.println(r1.addObject(r2).getLength());
+        System.out.println(r1.addObject(r2).getWidth());
 
     }
 }
