f517a44c9428f1e67a373f4dbf3be255a2a82ff5
alm35943 <malek.alsibai03@gmail.com>
2025-06-16 17:17:37 +0200

    Lecture 5 - passing an object to variable

---
+++ OOP_EL/11PassingObjectToVariableRectangleEX/src/Rectangle.java
@@ -0,0 +1,38 @@
+public class Rectangle {
+
+    // we have 2 attributes LENGTH and Width
+
+    private double length;     // private is used as data hiding
+    private double width;      // is an instance variable
+
+    public Rectangle() {    //creating a constructor with default values for length and width
+        length = 10;        // default values
+        width = 15;
+        System.out.println("a new room is created with " + length + " length and " + width + " width");
+    }
+    public Rectangle(double length, double width) {
+        this.length = length;
+        this.width = width;
+    }
+
+    public void setLength(double length) {
+        this.length = length;
+    }
+    public void setWidth(double width) {
+        this.width = width;
+    }
+
+    // we need 3 methods, one to return length, one to return width, one to return the area of the rectangle
+    public double getLength() {
+        return length;
+    }
+    public double getWidth() {
+        return width;
+    }
+    public double getArea() {
+        return length * width;
+    }
+
+
+}
+
