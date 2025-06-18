bffc5b2c365a6aa333cd8da481c81802b0b2989f
alm35943 <malek.alsibai03@gmail.com>
2025-06-18 21:12:00 +0200

    12 Comparing and copying objects

---
+++ OOP_EL/12ComparingAndCopyingObjects/src/Main.java
@@ -0,0 +1,14 @@
+public class Main {
+    public static void main(String[] args) {
+
+        Rectangle room1 = new Rectangle(10,50);
+        Rectangle room2 = new Rectangle(30, 70);
+        // to combine two objects r1 and r2
+        // Rectangle r3 =  r1.addObject(r2); //
+        System.out.println(room1.addObject(room2).getLength());
+        System.out.println(room1.addObject(room2).getWidth());
+
+    }
+}
+
+
