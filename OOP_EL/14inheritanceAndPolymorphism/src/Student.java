9392325003cbb067169c86b2391534be4dca9de1
alm35943 <malek.alsibai03@gmail.com>
2025-06-22 20:47:11 +0200

    14:Student class, getters and setters

--- OOP_EL/14inheritanceAndPolymorphism/src/Student.java
+++ OOP_EL/14inheritanceAndPolymorphism/src/Student.java
@@ -10,15 +10,38 @@ public class Student extends Person {
     public Student(){
     }
 
+    // instead of rewriting the code in the super class contructor we use the keyword SUPER
     public Student(String name, int age, String address, String nationality, int studyLevel, String specialization, double gpa) {
-        this.name = name;
-        this.age = age;
-        this.address = address;
-        this.nationality = nationality;
+        super(name, age, address, nationality);// to call the super constructor of the superclass, we pass the value of attributes in base class
         this.studyLevel = studyLevel;
         this.specialization = specialization;
         this.gpa = gpa;
     }
+
+    //Setters
+    public void setStudyLevel(int studyLevel) {
+        this.studyLevel = studyLevel;
+    }
+    public void setGpa(double gpa) {
+        this.gpa = gpa;
+    }
+    public void setSpecialization(String specialization) {
+        this.specialization = specialization;
+    }
+
+    // Getters
+    public int getStudyLevel() {
+        return studyLevel;
+    }
+    public double getGpa() {
+        return gpa;
+    }
+    public String getSpecialization() {
+        return specialization;
+    }
+
+
+
 
 
 
