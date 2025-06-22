57c8c52223c573867d9c19bda5f2d2661943e585
alm35943 <malek.alsibai03@gmail.com>
2025-06-22 20:21:14 +0200

    14: Constructors, setters and getters

--- OOP_EL/14inheritanceAndPolymorphism/src/Person.java
+++ OOP_EL/14inheritanceAndPolymorphism/src/Person.java
@@ -5,7 +5,45 @@ public class Person {
     private String address;
     private String nationality;
 
-    //
+    //Constructors
+    public Person(){
+
+    }
+    public Person(String name, int age, String address, String nationality) {
+        this.name = name;
+        this.age = age;
+        this.address = address;
+        this.nationality = nationality;
+    }
+    // Setters
+    public void setName(String name){
+        this.name = name;
+    }
+    public void setAge(int age){
+        this.age = age;
+    }
+    public void setAddress(String address){
+        this.address = address;
+    }
+    public void setNationality(String nationality){
+        this.nationality = nationality;
+    }
+    //Getters
+    public String getName(){
+        return name;
+    }
+    public int getAge(){
+        return age;
+    }
+    public String getAddress(){
+        return address;
+    }
+    public String getNationality(){
+        return nationality;
+    }
+
+
+
 
 }
 
