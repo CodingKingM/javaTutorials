1ffaec10491e70bdec0904910794769fc651c5cd
alm35943 <malek.alsibai03@gmail.com>
2025-06-23 19:48:05 +0200

    14, Vehicle Example. constructors, getters and setters in main

--- OOP_EL/14InheritanceRevisionVehicle/src/Vehicle.java
+++ OOP_EL/14InheritanceRevisionVehicle/src/Vehicle.java
@@ -1,3 +1,30 @@
 public class Vehicle {
 
+    String brand;
+    int year;
+
+    //constructor
+    public Vehicle(){
+        brand = "none";
+        year = 0;
+    }
+    public Vehicle(String brand, int year){
+        this.brand = brand;
+        this.year = year;
+    }
+    // setters
+    public void setBrand(String brand){
+        this.brand = brand;
+    }
+    public void setYear(int year){
+        this.year = year;
+    }
+    // Getter
+    public String getBrand(){
+        return brand;
+    }
+    public int getYear(){
+        return year;
+
+    }
 }
