a661ce2d0bf9f50ca7fcf1ee9a2b993f32c08fb9
alm35943 <malek.alsibai03@gmail.com>
2025-06-28 20:20:40 +0200

    17 - abstract method

--- OOP_EL/17AbstractClassesAndMethods/src/Main.java
+++ OOP_EL/17AbstractClassesAndMethods/src/Main.java
@@ -2,8 +2,8 @@ public class Main {
 
     public static void main(String[] args) {
 
-        Employee employee = new Employee();
-        System.out.println(employee);
+        //Employee employee = new Employee(); // Employee cannot create object as the class is abstracted
+
 
     }
 }
