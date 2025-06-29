d668386db60208e5883660c9f0540284de3bc892
alm35943 <malek.alsibai03@gmail.com>
2025-06-29 20:49:01 +0200

    19 enums switch case

--- OOP_EL/19EnumBroCODE/src/Main.java
+++ OOP_EL/19EnumBroCODE/src/Main.java
@@ -3,5 +3,18 @@ public class Main
     public static void main(String[] args)
     {
 
+        Day day = Day.THURSDAY;
+        System.out.println(day);
+        System.out.println(day.dayNumber());
+
+        switch(day){
+            case MONDAY,
+                 TUESDAY,
+                 WEDNESDAY,
+                 THURSDAY,
+                 FRIDAY -> System.out.println("It is a weekday");
+            case SATURDAY, SUNDAY -> System.out.println("It is a weekday");
+    }
+
     }
 }
