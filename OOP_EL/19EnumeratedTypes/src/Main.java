22c4151a93bb43c07ac516db8d7f8b9e0c467726
alm35943 <malek.alsibai03@gmail.com>
2025-06-29 20:36:44 +0200

    19 enums compareto, equals

--- OOP_EL/19EnumeratedTypes/src/Main.java
+++ OOP_EL/19EnumeratedTypes/src/Main.java
@@ -1,2 +1,16 @@
 public class Main {
+
+    public static void main(String[] args) {
+        Course c1 = Course.IME;
+        Course c2 = Course.ICS;
+
+//        System.out.println(c1.toString());
+//        System.out.println(c1.ordinal());
+
+        System.out.println(c1.compareTo(c2));  // It compares the ordinal value. c1 (IME) has an ordinale value of 1
+        System.out.println(c2.equals(c1));
+
+
+    }
+
 }
