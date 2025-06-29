c282eed8e96cee8f0e0a23cbc0116397878def8d
alm35943 <malek.alsibai03@gmail.com>
2025-06-29 20:52:35 +0200

    19 enums switch case

--- OOP_EL/19EnumBroCODE/src/Main.java
+++ OOP_EL/19EnumBroCODE/src/Main.java
@@ -1,7 +1,12 @@
+import java.util.Scanner;
+
 public class Main
 {
     public static void main(String[] args)
     {
+        Scanner input = new Scanner(System.in); // to accept user input
+        System.out.print("Enter a day of the week");
+        String dayOfWeek = input.nextLine().toUpperCase();
 
         Day day = Day.THURSDAY;
         System.out.println(day);
