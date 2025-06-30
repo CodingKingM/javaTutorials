7c4c7c684dcb55ed0e1e3424e5a997c34dd8a7ca
alm35943 <malek.alsibai03@gmail.com>
2025-06-30 20:55:40 +0200

    21 - arrays, implemented exception handeling when trying to access an array position outside of the index

--- OOP_EL/21ArraysBroCode/src/Main.java
+++ OOP_EL/21ArraysBroCode/src/Main.java
@@ -4,5 +4,19 @@ public class Main {
         // array used to store multiple values in one variable
 
         String[] cars = {"Mercedes", "BMW", "Nissan", "Ford"}; // starts with 0
+
+        cars[0] = "Mustang"; // we can manually assign it, Mercedes gets replaced with Mustang
+
+        // we can use exception handeling (optioinal) just to revise
+//        try {
+//            System.out.println(cars[4]);
+//        }
+//        catch (Exception e) {
+//            System.out.print("Error!" + e.getMessage());
+//        }
+
+        System.out.println(cars[3]);
+
+
     }
 }
