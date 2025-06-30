1a2a4f2aabddf9f2048c682590856f22bbdae7f3
alm35943 <malek.alsibai03@gmail.com>
2025-06-30 20:51:46 +0200

    21 - Arrays Bro code

---
+++ OOP_EL/21ArraysBroCode/src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+
+        // array used to store multiple values in one variable
+
+        String[] cars = {"Mercedes", "BMW", "Nissan", "Ford"}; // starts with 0
+    }
+}
