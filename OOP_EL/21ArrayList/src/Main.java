dd0e6f96c2181481148ac2a1ef2c128069a3c22f
alm35943 <malek.alsibai03@gmail.com>
2025-07-08 11:31:36 +0200

    21 -  ArrayList

---
+++ OOP_EL/21ArrayList/src/Main.java
@@ -0,0 +1,12 @@
+import java.util.ArrayList;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+    ArrayList<String> nameList = new ArrayList<String>(); // () set automatically to 10
+        nameList.add("malek");
+        nameList.add("sara");
+        System.out.println(nameList.get(0));
+    }
+}
--- OOP_EL/21ArraysBroCode/src/Main.java
+++ OOP_EL/21ArraysBroCode/src/Main.java
@@ -3,19 +3,29 @@ public class Main {
 
         // array used to store multiple values in one variable
 
-        String[] cars = {"Mercedes", "BMW", "Nissan", "Ford"}; // starts with 0
+//        String[] cars = {"Mercedes", "BMW", "Nissan", "Ford"}; // starts with 0
+//
+//        cars[0] = "Mustang"; // we can manually assign it, Mercedes gets replaced with Mustang
+//
+//        // we can use exception handeling (optioinal) just to revise
+////        try {
+////            System.out.println(cars[4]);
+////        }
+////        catch (Exception e) {
+////            System.out.print("Error!" + e.getMessage());
+////        }
+//
+//        System.out.println(cars[3]);
+        String[] cars = new String[4]; // assigning an array and allocating to it 7 spots
+        cars[0] = "Mercedes";
+        cars[1] = "BMW";
+        cars[2] = "Nissan";
+        cars[3] = "Ford";
+        System.out.println(cars[3]); // to display a single element of an array
+        for (int i = 0; i<cars.length; i++){
+            System.out.println(cars[i]);
+        }
 
-        cars[0] = "Mustang"; // we can manually assign it, Mercedes gets replaced with Mustang
-
-        // we can use exception handeling (optioinal) just to revise
-//        try {
-//            System.out.println(cars[4]);
-//        }
-//        catch (Exception e) {
-//            System.out.print("Error!" + e.getMessage());
-//        }
-
-        System.out.println(cars[3]);
 
 
     }
