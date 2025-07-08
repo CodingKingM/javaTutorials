9e9fb532db4a9a314ac0e1671e1083e488298b28
alm35943 <malek.alsibai03@gmail.com>
2025-07-08 11:46:00 +0200

    21 -  ArrayList (add,get,set,remove)

--- OOP_EL/21ArrayList/src/Main.java
+++ OOP_EL/21ArrayList/src/Main.java
@@ -4,9 +4,20 @@ public class Main {
 
     public static void main(String[] args) {
 
-    ArrayList<String> nameList = new ArrayList<String>(); // () set automatically to 10
+    ArrayList<String> nameList = new ArrayList<>(); // () set automatically to 10
         nameList.add("malek");
-        nameList.add("sara");
+        nameList.add("Alex");
+        System.out.println(nameList);
+        System.out.println("the Array list size is " + nameList.size());
         System.out.println(nameList.get(0));
+        System.out.println(nameList.get(1));
+        nameList.add(1,"Bob"); // adds an item at a specific index (it doesn't replace the existing one)
+        System.out.println(nameList);
+        System.out.println(nameList.get(2)); // ArrayList dynamically adds a new one
+        nameList.set(2, "Arios");
+        System.out.println(nameList);
+        nameList.remove(1);
+        System.out.println(nameList);
+        System.out.println(nameList.size());
     }
 }
